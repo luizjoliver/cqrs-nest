9890f73d2a2d3b5b64c5a6fbc44ff49fc8e50714
luizjoliver <luizfellipejoliver14@gmail.com>
2025-05-23 11:18:59 -0300

    Configuração .editorconfig , configuração do typeORM com sqlite  no projeto para facilitar desenvolvimento , configuração criação e exclusão DB e entidade employee

---
+++ src/entities/employee.entity.ts
@@ -0,0 +1,20 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Employee {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  name: string;
+  @ManyToOne(() => Employee, { onDelete: 'SET NULL' })
+  manager: Employee;
+
+  @JoinColumn()
+  managerId: number;
+}
