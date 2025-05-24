672aa8fa6556a94186beaf7a319f96c0f5537c28
luizjoliver <luizfellipejoliver14@gmail.com>
2025-05-24 11:29:22 -0300

    Criação primeiro command , create-employee e seu dto/handler

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,25 +7,31 @@ import {
   Param,
   NotFoundException,
 } from '@nestjs/common';
-import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { CreateEmployeeDto } from './commands/create-employee/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
-import { QueryBus } from '@nestjs/cqrs';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { GetEmployeeQuery } from './queries/get-employee/GetEmployeeQuery';
 import { plainToClass } from 'class-transformer';
 import { EmployeeDTO } from './queries/get-employee/employee.dto';
+import { CreateEmployeeCommand } from './commands/create-employee/create-employee.command';
 
 @Controller('employees')
 export class EmployeesController {
-  constructor(private readonly queryBus: QueryBus) {}
+  constructor(
+    private readonly queryBus: QueryBus,
+    private readonly commandBus: CommandBus,
+  ) {}
 
   @Post()
-  create(@Body() createEmployeeDto: CreateEmployeeDto) {
-    return null;
-  }
+  async create(@Body() dto: CreateEmployeeDto) {
+    const command = plainToClass(CreateEmployeeCommand, dto);
 
-  @Get()
-  findAll() {
-    return null;
+    const id: number = await this.commandBus.execute(command);
+
+    const query = plainToClass(GetEmployeeQuery, { id });
+    const employee: EmployeeDTO = await this.queryBus.execute(query);
+
+    return employee;
   }
 
   @Get(':id')
